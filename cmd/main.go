1df09f68bf7ca88c803fd857d65a257b6b192077
italoservio <italo.carvalho@pluto.tv>
2025-12-03 22:45:05 -0300

    chore: allow all origins

--- cmd/main.go
+++ cmd/main.go
@@ -31,12 +31,7 @@ func main() {
 	api.RegisterInfraRoutes(router)
 	api.RegisterCloakersRoutes(router, container)
 
-	corsOrigins := handlers.AllowedOrigins([]string{
-		"http://localhost:5173",
-		"http://localhost:5174",
-		"https://users.serviosoftware.com",
-		"https://ads.serviosoftware.com",
-	})
+	corsOrigins := handlers.AllowedOrigins([]string{"*"})
 	corsHeaders := handlers.AllowedHeaders([]string{"Content-Type", "Authorization"})
 	corsMethods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"})
 	corsCredentials := handlers.AllowCredentials()
